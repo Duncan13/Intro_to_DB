5e85ea30485c3c3813cce8e8f8fe46dee3a6bdcd
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-08-04 13:05:04 -0700

    added a book store sql file3

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,8 +30,8 @@ CREATE TABLE IF NOT EXISTS Orders(
 
 CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY order_id REFERENCES Orders,
-    FOREIGN KEY book_id REFERENCES Books,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
 
