1f1ffec7a4de1c3ec777fd20905fffca7c631179
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-08-04 12:57:36 -0700

    added a book store sql file1

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE Books(
+CREATE TABLE IF NOT EXISTS Books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
@@ -10,25 +10,25 @@ CREATE TABLE Books(
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Authors(
+CREATE TABLE IF NOT EXISTS Authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE Customers(
+CREATE TABLE IF NOT EXISTS Customers(
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     order_date DATE,
     FOREIGN KEY customer_id REFERENCES Customers
 );
 
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     FOREIGN KEY order_id REFERENCES Orders,
     FOREIGN KEY book_id REFERENCES Books,
